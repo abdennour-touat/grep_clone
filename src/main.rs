5ef4a2df8076734f01dfcfe35f5014879f74c057
abdennour touat <touatabdenour33@gmail.com>
2022-12-24 18:26:58 +0100

    adding case insensitiv option

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let conf = Config::new(&args).unwrap_or_else(|e| {
-        println!("{}", e);
+        eprintln!("{}", e);
         process::exit(1)
     });
     // println!("searching {:?}", conf.query);
     // println!("in file {:?}", conf.file_name);
     if let Err(err) = mini_grep::run(conf) {
-        println!("application error: {}", err);
+        eprintln!("application error: {}", err);
         process::exit(1);
     }
 }
