01607c7aba0de14ac724e4defc9b97b03fac0259
abdennour touat <touatabdenour33@gmail.com>
2022-12-27 19:41:07 +0100

    using iterators

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
+use mini_grep::run;
 use mini_grep::Config;
 use std::env;
 use std::process;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let conf = Config::new(&args).unwrap_or_else(|e| {
+    let conf = Config::new(env::args()).unwrap_or_else(|e| {
         eprintln!("{}", e);
         process::exit(1)
     });
-    // println!("searching {:?}", conf.query);
-    // println!("in file {:?}", conf.file_name);
-    if let Err(err) = mini_grep::run(conf) {
+    if let Err(err) = run(conf) {
         eprintln!("application error: {}", err);
         process::exit(1);
     }
